55e15ffe3121984b08b353dc16eeebf17c3e0828
camila ferreira <mila.e.ferreira28@gmail.com>
2022-10-27 16:54:00 -0300

    arrumando erros

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from core.models import Usuario, Vaga, Formação, Cidade, Profissional, Inscrição
+from core.models import Usuario, Vaga, Formação, Cidade, Profissional, Inscrição, Expreriencias
 
 admin.site.register(Usuario)
 admin.site.register(Vaga)
@@ -8,3 +8,4 @@ admin.site.register(Formação)
 admin.site.register(Cidade)
 admin.site.register(Profissional)
 admin.site.register(Inscrição)
+admin.site.register(Expreriencias)
--- core/models.py
+++ core/models.py
@@ -1,22 +1,41 @@
-
 from django.db import models
+
+
+class Cidade(models.Model):
+    nome_cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nome_cidade} - {self.estado}"
+
 
 class Usuario(models.Model):
     nome = models.CharField(max_length=100)
     senha = models.CharField(max_length=100)
     endereco = models.CharField(max_length=100)
     email = models.EmailField()
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT, related_name="+")
 
     def __str__(self):
-        return f'{self.nome} - {self.endereco} - {self.email}'
+        return f"{self.nome} - {self.endereco} - {self.email}"
+
 
 class Vaga(models.Model):
     descricao = models.CharField(max_length=40)
     titulo = models.CharField(max_length=80)
     status = models.CharField(max_length=100)
-    
+
     def __str__(self):
-        return f'{self.descricao} - {self.titulo} - {self.status}'
+        return f"{self.descricao} - {self.titulo} - {self.status}"
+
+
+class Inscrição(models.Model):
+    descricao = models.CharField(max_length=100)
+    vaga = models.ForeignKey(Vaga, on_delete=models.PROTECT, related_name="+")
+
+    def __str__(self):
+        return f"{self.descricao} - {self.vaga}"
+
 
 class Formação(models.Model):
     nome_instituicao = models.CharField(max_length=100)
@@ -27,29 +46,26 @@ class Formação(models.Model):
     descricao_formacao = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.formacao} - {self.trabalho}'
+        return f"{self.formacao} - {self.trabalho}"
 
-class Cidade(models.Model):
-    nome_cidade = models.CharField(max_length=100)
-    estado = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f'{self.nome_cidade} - {self.estado}'
 
 class Profissional(models.Model):
-    cnpj = models.CharField(max_length=32, null=True, blank=True)
+    CPF = models.CharField(max_length=32, null=True, blank=True)
     estado_civil = models.CharField(max_length=100)
     data_nascimento = models.DateField(blank=True, null=True)
     twitter = models.CharField(max_length=100, blank=True, null=True)
     linkedin = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return f'{self.cnpj} - {self.data_nascimento} - {self.estado_civil}'
+        return f"{self.CPF} - {self.data_nascimento} - {self.estado_civil}"
 
-class Inscrição(models.Model):
-    descricao = models.CharField(max_length=100)
+class Expreriencias(models.Model):
+    descricao_experiencia = models.CharField(max_length=32)
+    data_inicio = models.DateField(blank=True, null=True)
+    data_termino = models.DateField(blank=True, null=True)
+    experiencias1 = models.TextField(max_length=200)
+    experiencias2 = models.TextField(max_length=32)
+    experiencias3 = models.TextField(max_length=32)
 
     def __str__(self):
-        return f'{self.descricao}'
-
-
+        return f"{self.descricao_experiencia} - {self.experiencias1}"
